afe81a994ce1c2641bd5cec7655c3752bfdd0568
cwmiller6 <cmiller2019@pingry.org>
2016-03-09 12:16:52 -0500

    Second Commit. This is for the re-factoring assignmetn and now includes an enchancments of a while loop.

--- coinSim.py
+++ coinSim.py
@@ -7,9 +7,18 @@ import random
 #Make a single variable 'result' and use the random.randint function to set it between 1 and 2
 result = random.randint(1, 2)
 
-#if the random int selected is 1, this will represent heads and will allow program to print heads
-if result == 1:
-	print("The coin lands on heads")
-#if the random int selected is 2, this will represent tails and will allow program to print tails
-if result == 2:
-	print("The coin lands on tails") 
+#Enchancment for while loop (Re-Factoring Git Assignment): 
+spin = True
+
+while spin == True:
+	again = raw_input("Would you like to flip again? (y or n)\n>".lower())
+	if again == 'y':
+		#if the random int selected is 1, this will represent heads and will allow program to print heads
+		if result == 1:
+			print("The coin lands on heads")
+		#if the random int selected is 2, this will represent tails and will allow program to print tails
+		if result == 2:
+			print("The coin lands on tails") 
+	if again == 'n':
+		print("Thanks for flipping!")
+		spin = False 
